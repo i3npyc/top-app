a1f842fe0ea2e3566ff83abb2b2a909ff08e2282
Stanislav <i3npyc09@gmail.com>
2022-02-28 18:33:12 +0300

    feat: start laying out basement

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -4,5 +4,13 @@ import { FooterProps } from './FooterProps';
 import styles from './Footer.module.css';
 
 export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>;
+  return (
+    <div {...props}>
+      <div className={styles.copyright}>
+        OwlTop © 2020 - 2022 Все права защищены
+      </div>
+      <div className={styles.agreement}>Пользовательское соглашение</div>
+      <div className={styles.politics}>Политика конфиденциальности</div>
+    </div>
+  );
 };
